2c20773795077728d810970e35a5edcba597dcb5
xxshanshui <149645305+xxshanshui@users.noreply.github.com>
2023-11-30 22:17:50 +0800

    Add files via upload

---
+++ 前端项目从0/aqq/dist/assets/js/about.js
@@ -0,0 +1,82 @@
+"use strict";
+(self["webpackChunkaqq"] = self["webpackChunkaqq"] || []).push([["about"],{
+
+/***/ "./src/views/AboutView.vue":
+/*!*********************************!*\
+  !*** ./src/views/AboutView.vue ***!
+  \*********************************/
+/***/ (function(__unused_webpack_module, __webpack_exports__, __webpack_require__) {
+
+__webpack_require__.r(__webpack_exports__);
+/* harmony import */ var _AboutView_vue_vue_type_template_id_4cebd208__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ./AboutView.vue?vue&type=template&id=4cebd208 */ "./src/views/AboutView.vue?vue&type=template&id=4cebd208");
+/* harmony import */ var _node_modules_vue_vue_loader_v15_lib_runtime_componentNormalizer_js__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! !../../node_modules/@vue/vue-loader-v15/lib/runtime/componentNormalizer.js */ "./node_modules/@vue/vue-loader-v15/lib/runtime/componentNormalizer.js");
+
+var script = {}
+
+
+/* normalize component */
+;
+var component = (0,_node_modules_vue_vue_loader_v15_lib_runtime_componentNormalizer_js__WEBPACK_IMPORTED_MODULE_1__["default"])(
+  script,
+  _AboutView_vue_vue_type_template_id_4cebd208__WEBPACK_IMPORTED_MODULE_0__.render,
+  _AboutView_vue_vue_type_template_id_4cebd208__WEBPACK_IMPORTED_MODULE_0__.staticRenderFns,
+  false,
+  null,
+  null,
+  null
+  
+)
+
+/* hot reload */
+if (false) { var api; }
+component.options.__file = "src/views/AboutView.vue"
+/* harmony default export */ __webpack_exports__["default"] = (component.exports);
+
+/***/ }),
+
+/***/ "./src/views/AboutView.vue?vue&type=template&id=4cebd208":
+/*!***************************************************************!*\
+  !*** ./src/views/AboutView.vue?vue&type=template&id=4cebd208 ***!
+  \***************************************************************/
+/***/ (function(__unused_webpack_module, __webpack_exports__, __webpack_require__) {
+
+__webpack_require__.r(__webpack_exports__);
+/* harmony export */ __webpack_require__.d(__webpack_exports__, {
+/* harmony export */   render: function() { return /* reexport safe */ _node_modules_babel_loader_lib_index_js_clonedRuleSet_40_use_0_node_modules_vue_vue_loader_v15_lib_loaders_templateLoader_js_ruleSet_1_rules_3_node_modules_vue_cli_service_lib_config_vue_loader_v15_resolve_compat_vue_loader_js_vue_loader_options_AboutView_vue_vue_type_template_id_4cebd208__WEBPACK_IMPORTED_MODULE_0__.render; },
+/* harmony export */   staticRenderFns: function() { return /* reexport safe */ _node_modules_babel_loader_lib_index_js_clonedRuleSet_40_use_0_node_modules_vue_vue_loader_v15_lib_loaders_templateLoader_js_ruleSet_1_rules_3_node_modules_vue_cli_service_lib_config_vue_loader_v15_resolve_compat_vue_loader_js_vue_loader_options_AboutView_vue_vue_type_template_id_4cebd208__WEBPACK_IMPORTED_MODULE_0__.staticRenderFns; }
+/* harmony export */ });
+/* harmony import */ var _node_modules_babel_loader_lib_index_js_clonedRuleSet_40_use_0_node_modules_vue_vue_loader_v15_lib_loaders_templateLoader_js_ruleSet_1_rules_3_node_modules_vue_cli_service_lib_config_vue_loader_v15_resolve_compat_vue_loader_js_vue_loader_options_AboutView_vue_vue_type_template_id_4cebd208__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! -!../../node_modules/babel-loader/lib/index.js??clonedRuleSet-40.use[0]!../../node_modules/@vue/vue-loader-v15/lib/loaders/templateLoader.js??ruleSet[1].rules[3]!../../node_modules/@vue/cli-service/lib/config/vue-loader-v15-resolve-compat/vue-loader.js??vue-loader-options!./AboutView.vue?vue&type=template&id=4cebd208 */ "./node_modules/babel-loader/lib/index.js??clonedRuleSet-40.use[0]!./node_modules/@vue/vue-loader-v15/lib/loaders/templateLoader.js??ruleSet[1].rules[3]!./node_modules/@vue/cli-service/lib/config/vue-loader-v15-resolve-compat/vue-loader.js??vue-loader-options!./src/views/AboutView.vue?vue&type=template&id=4cebd208");
+
+
+/***/ }),
+
+/***/ "./node_modules/babel-loader/lib/index.js??clonedRuleSet-40.use[0]!./node_modules/@vue/vue-loader-v15/lib/loaders/templateLoader.js??ruleSet[1].rules[3]!./node_modules/@vue/cli-service/lib/config/vue-loader-v15-resolve-compat/vue-loader.js??vue-loader-options!./src/views/AboutView.vue?vue&type=template&id=4cebd208":
+/*!**********************************************************************************************************************************************************************************************************************************************************************************************************************************!*\
+  !*** ./node_modules/babel-loader/lib/index.js??clonedRuleSet-40.use[0]!./node_modules/@vue/vue-loader-v15/lib/loaders/templateLoader.js??ruleSet[1].rules[3]!./node_modules/@vue/cli-service/lib/config/vue-loader-v15-resolve-compat/vue-loader.js??vue-loader-options!./src/views/AboutView.vue?vue&type=template&id=4cebd208 ***!
+  \**********************************************************************************************************************************************************************************************************************************************************************************************************************************/
+/***/ (function(__unused_webpack_module, __webpack_exports__, __webpack_require__) {
+
+__webpack_require__.r(__webpack_exports__);
+/* harmony export */ __webpack_require__.d(__webpack_exports__, {
+/* harmony export */   render: function() { return /* binding */ render; },
+/* harmony export */   staticRenderFns: function() { return /* binding */ staticRenderFns; }
+/* harmony export */ });
+var render = function render() {
+  var _vm = this,
+    _c = _vm._self._c;
+  return _vm._m(0);
+};
+var staticRenderFns = [function () {
+  var _vm = this,
+    _c = _vm._self._c;
+  return _c("div", {
+    staticClass: "about"
+  }, [_c("h1", [_vm._v("This is an about page")])]);
+}];
+render._withStripped = true;
+
+
+/***/ })
+
+}]);
+//# sourceMappingURL=about.js.map
--- 前端项目从0/aqq/vue.config.js
+++ 前端项目从0/aqq/vue.config.js
@@ -1,4 +1,108 @@
 const { defineConfig } = require("@vue/cli-service")
+const path = require("path")
+function resolve(dir) {
+	return path.join(__dirname, dir)
+}
 module.exports = defineConfig({
-  transpileDependencies: true
+	transpileDependencies: true,
+	lintOnSave: false,
+	publicPath: "/",
+	outputDir: "dist",
+	assetsDir: "assets",
+	devServer: {
+		host: "0.0.0.0",
+		https: false,
+		open: true,
+
+
+
+		proxy: {
+
+			[process.env.VUE_APP_IDENT]: {
+				changeOrigin: true,
+				secure: false,
+				target: process.env.VUE_APP_URL,
+				pathRewrite: {
+					["^" + process.env.VUE_APP_IDENT]: ""
+
+				}
+			}
+		},
+	},
+
+	configureWebpack: {
+		resolve: {
+			alias: {
+				"@": resolve("src"),
+				$components: resolve("src/components"),
+				vue$: "vue/dist/vue.esm.js"
+			}
+		}
+	},
+	chainWebpack(config) {
+		config.plugins.delete("preload")
+		config.plugins.delete("prefetch")
+		config.module.rule("svg").exclude.add(resolve("src/icons")).end()
+		config.module
+			.rule("icons")
+			.test(/\.svg$/)
+			.include.add(resolve("src/icons"))
+			.end()
+			.use("svg-sprite-loader")
+			.loader("svg-sprite-loader")
+			.options({
+				symbolId: "icon-[name]"
+			})
+			.end()
+		config.module
+			.rule("vue")
+			.use("vue-loader")
+			.loader("vue-loader")
+			.tap((options) => {
+				options.compilerOptions.preserveWhitespace = true
+				return options
+			})
+			.end()
+
+		config.when(process.env.NODE_ENV === "development", (config) => config.devtool("cheap-source-map"))
+
+		config.when(process.env.NODE_ENV !== "development", (config) => {
+			config
+				.plugin("ScriptExtHtmlWebpackPlugin")
+				.after("html")
+				.use("script-ext-html-webpack-plugin", [
+					{
+
+						inline: /runtime\..*\.js$/
+					}
+				])
+				.end()
+			config.optimization.splitChunks({
+				chunks: "all",
+				cacheGroups: {
+					libs: {
+						name: "chunk-libs",
+						test: /[\\/]node_modules[\\/]/,
+						priority: 10,
+						chunks: "initial"
+					},
+					elementUI: {
+						name: "chunk-elementUI",
+						priority: 20,
+						test: /[\\/]node_modules[\\/]element-ui[\\/]/
+					},
+					commons: {
+						name: "chunk-commons",
+						test: resolve("src/components"),
+						minChunks: 3,
+						priority: 5,
+						reuseExistingChunk: true
+					}
+				}
+			})
+			config.optimization.runtimeChunk("single")
+
+		})
+
+	}
 })
